1f8569204514a123834e7865d7b4a6ae9c3872a1
Andrew <andrewsfei@163.com>
2019-09-19 09:57:41 +0800

    1、启动页解决白屏 在js中关闭

--- App.js
+++ App.js
@@ -7,25 +7,33 @@
  */
 
 import React from 'react';
-import { View, Text, Button } from 'react-native';
-import { createAppContainer, createStackNavigator, StackActions, NavigationActions } from 'react-navigation';
+import {View, Text, Button} from 'react-native';
+import {createAppContainer, createStackNavigator, StackActions, NavigationActions} from 'react-navigation';
 import HomeScreen from "./index";
 import LISTScreen from "./studyrn/listview";
 import GIRDScreen from "./studyrn/girdview";
 import FATHER from './studyrn/Father'
+// export default NativeModules.SplashScreen;
+const RootStack = createStackNavigator({
 
-const AppNavigator = createStackNavigator({
+
     Home: {
         screen: LISTScreen,
     },
     Gird: {
         screen: GIRDScreen,
     },
-    Father:{
-        screen:FATHER
+    Father: {
+        screen: FATHER
     }
 }, {
-        initialRouteName: 'Home',
-    });
-    
-export default createAppContainer(AppNavigator);
+    initialRouteName: 'Home',
+    headerMode: 'none'
+});
+//修改前
+// export default createAppContainer(RootStack );
+//修改后
+const App = createAppContainer(RootStack);
+export default App
+
+
--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import { createAppContainer, createStackNavigator, StackActions, NavigationActions } from 'react-navigation';
-import React, { Component } from 'react';
+import React, { Component,NativeModules } from 'react';
 import { AppRegistry } from 'react-native';
 import { name as appName } from './app.json';
 import App from './App.js'
@@ -12,16 +12,16 @@ import LoginView from './studyrn/loginView';
 import ViewPageScollView from './studyrn/viewPageScrollView';
 import GirdView from './studyrn/girdview';
 import FlatLists from './studyrn/listview';
-
+import SplashScreen from 'react-native-splash-screen'
 //Es5的引入外部布局
 // var LoginView = require('./studyrn/loginView');
 
 
 
 class include extends Component {
-    
+
     render() {
-        
+
         return (
             // <LoginView/>
             //<ViewPageScollView/>//GirdView页面
@@ -29,7 +29,13 @@ class include extends Component {
             <FlatLists />//ListView页面
         );
     }
-    
+
+    componentDidMount() {
+        // do stuff while splash screen is shown
+        // After having done stuff (such as async tasks) hide the splash screen
+        SplashScreen.hide();
+    }
+
 }
 
 // appName 为原生中的getName 中return的值
